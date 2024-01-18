dee77e7d8733dd516b925793fe226c2813257662
Natan-ok <bogdannatan@gmail.com>
2024-01-18 21:49:49 +0200

    1
    1

--- AlcoCalc.py
+++ AlcoCalc.py
@@ -16,67 +16,72 @@ cfcs = check_for_custom_string
 class AlcoCalc:
     intoxication_levels = {"sober": 0, "tipsy": 0.5, "drunk": 1, "very drunk": 2, "blackout": 3}
     #C=A/(m*r)
-    class Drink_types:
-        drink_types = {
-                1: {"name": "beer", "alcohol_content": 0.05},
-                2: {"name": "wine", "alcohol_content": 0.12},
-                3: {"name": "whiskey", "alcohol_content": 0.4},
-                4: {"name": "kvass", "alcohol_content": 0.02},
-                5: {"name": "vodka", "alcohol_content": 0.4},
-                6: {"name": "champagne", "alcohol_content": 0.12},
-                7: {"name": "cognac", "alcohol_content": 0.4},
-                8: {"name": "rum", "alcohol_content": 0.4},
-                9: {"name": "gin", "alcohol_content": 0.4},
-                10: {"name": "tequila", "alcohol_content": 0.4},
-                11: {"name": "absinthe", "alcohol_content": 0.7},
-                12: {"name": "sambuca", "alcohol_content": 0.4},
-                13: {"name": "martini", "alcohol_content": 0.15},
-                14: {"name": "vermouth", "alcohol_content": 0.18},
-                15: {"name": "campari", "alcohol_content": 0.25},
-                16: {"name": "jagermeister", "alcohol_content": 0.35},
-                17: {"name": "absolut", "alcohol_content": 0.4},
-                18: {"name": "jack daniels", "alcohol_content": 0.4},
-                19: {"name": "jim beam", "alcohol_content": 0.4},
-                20: {"name": "bacardi", "alcohol_content": 0.4},
-                21: {"name": "malibu", "alcohol_content": 0.21},
-                22: {"name": "baileys", "alcohol_content": 0.17},
-                23: {"name": "sambuca", "alcohol_content": 0.4},
-                24: {"name": "sambuca", "alcohol_content": 0.4},
+
+    drink_types = {
+        1: {"name": "beer", "alcohol_content": 0.05},
+        2: {"name": "wine", "alcohol_content": 0.12},
+        3: {"name": "whiskey", "alcohol_content": 0.4},
+        4: {"name": "kvass", "alcohol_content": 0.02},
+        5: {"name": "vodka", "alcohol_content": 0.4},
+        6: {"name": "champagne", "alcohol_content": 0.12},
+        7: {"name": "cognac", "alcohol_content": 0.4},
+        8: {"name": "rum", "alcohol_content": 0.4},
+        9: {"name": "gin", "alcohol_content": 0.4},
+        10: {"name": "tequila", "alcohol_content": 0.4},
+        11: {"name": "absinthe", "alcohol_content": 0.7},
+        12: {"name": "sambuca", "alcohol_content": 0.4},
+        13: {"name": "martini", "alcohol_content": 0.15},
+        14: {"name": "vermouth", "alcohol_content": 0.18},
+        15: {"name": "campari", "alcohol_content": 0.25},
+        16: {"name": "jagermeister", "alcohol_content": 0.35},
+        17: {"name": "absolut", "alcohol_content": 0.4},
+        18: {"name": "jack daniels", "alcohol_content": 0.4},
+        19: {"name": "jim beam", "alcohol_content": 0.4},
+        20: {"name": "bacardi", "alcohol_content": 0.4},
+        21: {"name": "malibu", "alcohol_content": 0.21},
+        22: {"name": "baileys", "alcohol_content": 0.17},
+        23: {"name": "sambuca", "alcohol_content": 0.4},
+        24: {"name": "samagon", "alcohol_content": 0.95},
+
 
             }
 
 
-    """ def сalculate_alcohol_by_volume(drink_types, a, drink_amount, weight=65, height=175, gender="male", fullness=True) -> float:
-        vm = 2.447 + 0.3362*weight + 10.74*height
-        vf = 2.097 + 0.2466 * weight + 10.69 * height
-        p = 0.51 if fullness else 0.55
-        if gender == 'male':
-            vm = 2.447 + 0.3362*weight + 10.74*height
-            c = ((drink_types[a]["alcohol_content"])*drink_amount*p)/vm
-        else:
-            vf = 2.097 + 0.2466 * weight + 10.69 * height
-            c = ((drink_types[a]["alcohol_content"])*drink_amount*p)/vf
-        result = c
-        return result"""
-    def сalculate_alcohol_by_volume(drinktype, drink_amount, weight=65, height=175, gender="male", fullness=True) -> float:
-        vm = 2.447 + 0.3362*weight + 10.74*height
-        vf = 2.097 + 0.2466 * weight + 10.69 * height
-        p = 0.51 if fullness else 0.55
-        if gender == 'male':
-            vm = 2.447 + 0.3362*weight + 10.74*height
-            c = (AlcoCalc.Drink_types.drink_types[drinktype]["alcohol_content"]*drink_amount*p)/vm
-        else:
-            vf = 2.097 + 0.2466 * weight + 10.69 * height
-            c = (AlcoCalc.Drink_types.drink_types[drinktype]["alcohol_content"]*drink_amount*p)/vf
-        result = c
+
+    def сalculate_alcohol_by_volume(drinktype, drink_amount, weight=65, height=175, gender="male", fullness=True) -> float: 
+        k = 1 if fullness == False else 10/17
+        bmi = weight/((height/100)**2)
+        a = drink_amount*AlcoCalc.drink_types[drinktype]["alcohol_content"]*0.789
+        rm = 1.0181-0.0113*bmi
+        rf = 0.9367-0.01240*bmi
+        r = rm if gender == "male" else rf
+        bac = a/(weight*r)*k
+        result = bac
         return result
 
     def сalculate_alcohol_effect(c) -> dict:
-        pass # Your code here
-        
-        result = {"promiles": float, "intoxication_level": str, "time_to_sober_minutes": int, "can_drive": bool}
+        can_drive = False
+        if c < 0.2:
+            can_drive = True
+            intoxication_level = "\nsober"
+        elif c < 0.59:
+            intoxication_level = "\nПоведение:\n*Средневыраженная\n*эйфория\n*РасслаблениеОщущение\n*радости\n*Говорливость\n*Понижение\n*сдержанности\n" + "Нарушения:\n*Концентрация\n"
+        elif c < 0.9:
+            intoxication_level = "\nПоведение:\n*Притупление ощущения\n*Расторможенность\n*Экстравертность\n" + "Нарушения:\n*Рассуждение\n*Глубина восприятия\n*Периферическое зрение\n*Приспособление зрачка к свету\n" 
+        elif c < 1.9:
+            intoxication_level = "\nПоведение:\n*Сверх-экспрессивность\n*Переменчивость эмоций\n*Гнев или печаль\n*Неистовость\n*Снижение либидо\n" + "Нарушения:\n*Рефлексы\n*Время реакции\n*Основные моторные навыки\n*Способность к контролю движения (появляется шатающаяся походка)\n*Нечленораздельная речь\n*Эрекция (у мужчин, временно)\n*Вероятность временного алкогольного отравления\n"
+        elif c < 2.9:
+            intoxication_level = "\nПоведение:\n*Ступор\n*Потеря способности к пониманию\n*Ослабление способностей к ощущению\n" +"Нарушения:\n*Вероятность потери сознания\n*Тяжелое нарушение моторики\n*Потеря памяти" 
+        elif c < 3.9:
+            intoxication_level = "\nПоведение:\n*Сильное угнетение функций центральной нервной системы\n*Потеря сознания\n*Вероятность смерти\n" + "Нарушеняи:\n*Контроль над мочеиспусканием\n*Дыхание\n*чувство равновесия (полная утрата)\n*Сердцебиение\n"
+        elif c < 5:
+            intoxication_level = "\nПоведение:\n*Полная утрата контроля за поведением\n*Потеря сознания\n*Вероятность смерти\n" + "Нарушения:\n*Дыхание\n*Сердцебиение\n*Контроль над движением зрачков (Нистагм)\n"
+        else:
+            intoxication_level = "\nПоведение:\n*Высокий риск отравления\n*Высокий риск смерти"
+        time_to_sober_hours = c*60/0.15/60
+
+        result = {"promiles": c, "intoxication_level": intoxication_level, "time_to_sober_minutes": time_to_sober_hours, "can_drive": can_drive}
         return result
-    
 class Drinker:
     attributeList = ["weight", "height", "gender", "age", "fullness"]
     
--- ConsoleAlcoPy.py
+++ ConsoleAlcoPy.py
@@ -1,4 +1,8 @@
 from AlcoCalc import AlcoCalc, Drinker
+from colorama import Fore
+import os
+os.system('cls')
+print(Fore.GREEN)
 
 UserDict = {}
 
@@ -7,21 +11,23 @@ UserDict = {a: None for a in Drinker.attributeList}
 print(UserDict)
 for a in Drinker.attributeList:
     UserDict[a] = input(f"Enter {a}: ")
-#def сalculate_alcohol_by_volume(drinktype, drink_amount, weight=65, height=175, gender="male", fullness=True) -> float:
 User = Drinker(**UserDict)
+#User = Drinker(60, 169,'male', 17, True)
 print("\n" + str(User))
-for i in AlcoCalc.Drink_types.drink_types:
-    print(f"{i}: {AlcoCalc.Drink_types.drink_types[i]['name']}")
+
+for i in AlcoCalc.drink_types:
+    print(f"{i}: {AlcoCalc.drink_types[i]['name']} {AlcoCalc.drink_types[i]['alcohol_content']}%")
+
 def askdrinktype():
     check = int(input("Enter drink type: "))
-    if  check in AlcoCalc.Drink_types.drink_types:
+    if  check in AlcoCalc.drink_types:
         global drinktype
         drinktype = check
 
     else:
         print("Wrong drink type")
         askdrinktype()
-
+#1919.5242
 askdrinktype()
 amount = int(input("Enter drink amount: "))
 
@@ -29,4 +35,7 @@ amount = int(input("Enter drink amount: "))
 
 
 c = AlcoCalc.сalculate_alcohol_by_volume(drinktype,amount,User.weight,User.height,User.gender,User.fullness)
-print(c)
+result = AlcoCalc.сalculate_alcohol_effect(c)
+for i in result:
+    print(f"{i}: {result[i]}")
+
