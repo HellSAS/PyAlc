00aa2cafee1e0aeeb159349cf69aafe033b0e1d9
HellSAS <ponomarevartur925@gmail.com>
2024-01-21 20:39:58 +0200

    123

--- testpy.py
+++ testpy.py
@@ -1,5 +1,9 @@
-import inspect
-import art
-import colorama
+import click
 
-print(inspect.getfile(colorama))
+@click.command()
+@click.confirmation_option(prompt='Are you sure you want to drop the db?')
+def hello(name):
+    click.echo(f"Hello, {name}!")
+
+if __name__ == '__main__':
+    hello()
