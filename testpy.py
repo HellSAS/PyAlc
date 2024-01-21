adf633c70d6eaa04fc44a383af754f1009bb77b2
Natan-ok <bogdannatan@gmail.com>
2024-01-21 18:38:58 +0200

    1
    1

--- testpy.py
+++
@@ -1,9 +0,0 @@
-import click
-
-@click.command()
-@click.confirmation_option(prompt='Are you sure you want to drop the db?')
-def hello(name):
-    click.echo(f"Hello, {name}!")
-
-if __name__ == '__main__':
-    hello()
