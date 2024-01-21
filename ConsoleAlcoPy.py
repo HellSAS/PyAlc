38b7bd363d892820d488bbeb5c99a1aa59f8cd56
Natan-ok <bogdannatan@gmail.com>
2024-01-21 17:12:15 +0200

    fix code and rework feature
    AlcoCalc-
    fix - if the amount of alcohol is more than 10,000, then now it will not be a negative number but 9999
    
    fix - Now if the amount of alcohol in the blood is too high it will say "death"
    
    rework - now instead of a table you will be asked the amount of alcohol in the drink
    
    feature - Now you can understand in what format to enter data
    
    ConsoleAlcoCalc-
    
    feature - Now you can understand in what format to enter data

--- ConsoleAlcoPy.py
+++ ConsoleAlcoPy.py
@@ -22,11 +22,11 @@ def work():
     def userask():
         UserDict = {}
         UserDict = {a: None for a in Drinker.attributeList}
-        userask = (Fore.CYAN + 'Enter weight' + Style.RESET_ALL, 
-                   Fore.CYAN + 'Enter height' + Style.RESET_ALL, 
-                   Fore.CYAN + 'Enter gender' + Style.RESET_ALL, 
-                   Fore.CYAN + 'Enter age' + Style.RESET_ALL, 
-                   Fore.CYAN + 'Enter fullness' + Style.RESET_ALL)
+        userask = (Fore.CYAN + 'Enter weight(kg)' + Style.RESET_ALL, 
+                   Fore.CYAN + 'Enter height(cm)' + Style.RESET_ALL, 
+                   Fore.CYAN + 'Enter gender(male/female)' + Style.RESET_ALL, 
+                   Fore.CYAN + 'Enter age(not necessary)' + Style.RESET_ALL, 
+                   Fore.CYAN + 'Enter fullness(true/false)' + Style.RESET_ALL)
         for a in Drinker.attributeList:
             i = Drinker.attributeList.index(a)
             UserDict[a] = input(f"{userask[i]}: ")
@@ -35,7 +35,7 @@ def work():
         print("\n" + str(User) + "\n\n\n")
 
     def askdrinktype():
-        print(Fore.BLUE + "Choose drink type:\n" + Style.RESET_ALL)
+        """print(Fore.BLUE + "Choose drink type:\n" + Style.RESET_ALL)
         
         for i in AlcoCalc.drink_types:
             print(f"{Fore.YELLOW}{i}{Style.RESET_ALL}: {AlcoCalc.drink_types[i]['name']} {AlcoCalc.drink_types[i]['alcohol_content']}%")
@@ -49,7 +49,7 @@ def work():
                 askdrinktype1()
         
         try:
-            check = int(input(Fore.CYAN + "\n\nEnter drink type: " + Style.RESET_ALL))
+            check = int(input(Fore.CYAN + "\n\nEnter drink type(num of drink type): " + Style.RESET_ALL))
         except:
             print(Fore.RED + "Wrong drink type, enter drink type again:" + Style.RESET_ALL)
             askdrinktype1()
@@ -59,19 +59,31 @@ def work():
             drinktype = check
         else:
             print(Fore.RED + "Wrong drink type, enter drink type again:" + Style.RESET_ALL)
+            askdrinktype1()"""
+        global alcohol_content
+        def askdrinktype1():
+            global alcohol_content
+            try:
+                alcohol_content = float(input())
+            except:
+                askdrinktype1()
+        try:
+            print(Fore.BLUE + "write the amount of alcohol in the drink:\nin the format 0.(%) - number of percents:\n\n" + Style.RESET_ALL)
+            alcohol_content = float(input())
+        except:
+            print(Fore.RED + "Wrong drink type, enter drink type again:" + Style.RESET_ALL)
             askdrinktype1()
-
         global amount
         def askdrinktype2():
             global amount
 
             try:
-                amount = int(input())
+                amount = float(input())
             except:
                 askdrinktype2()
 
         try:
-            amount = int(input(Fore.CYAN + "\n\nEnter drink amount: " + Style.RESET_ALL))
+            amount = float(input(Fore.CYAN + "\n\nEnter drink amount: " + Style.RESET_ALL))
         except:
             print(Fore.RED + "Wrong drink amount, enter drink amount again:" + Style.RESET_ALL)
             askdrinktype2()
@@ -82,7 +94,7 @@ def work():
 
     def resultate():
         print(Fore.BLUE + "\n\n\n\nYour result:" + Style.RESET_ALL)
-        c = AlcoCalc.calculate_alcohol_by_volume(drinktype, amount, User.weight, User.height, User.gender, User.fullness)
+        c = AlcoCalc.calculate_alcohol_by_volume(alcohol_content, amount, User.weight, User.height, User.gender, User.fullness)
         result = AlcoCalc.calculate_alcohol_effect(c)
         for i in result:
             if i == "intoxication_level":
---
+++ testpy.py
@@ -0,0 +1,6 @@
+infinity = float("inf")
+print(infinity)
+if 9999999 > infinity:
+    print("True")
+else:   
+    print("False")
