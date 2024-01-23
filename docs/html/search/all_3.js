b22fe83a72543fd043329936210adf58fbc4d628
HellSAS <ponomarevartur925@gmail.com>
2024-01-23 18:49:39 +0200

    123

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['drink_5ftypes_0',['drink_types',['../class_alco_calc_1_1_alco_calc.html#a54ff62e948594aa8e08a989a7bc3056b',1,'AlcoCalc::AlcoCalc']]],
-  ['drinker_1',['Drinker',['../class_alco_calc_1_1_drinker.html',1,'AlcoCalc']]]
+  ['drinker_0',['Drinker',['../class_alco_calc_1_1_drinker.html',1,'AlcoCalc']]]
 ];
--- docs/html/search/variables_0.js
+++ docs/html/search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['drink_5ftypes_0',['drink_types',['../class_alco_calc_1_1_alco_calc.html#a54ff62e948594aa8e08a989a7bc3056b',1,'AlcoCalc::AlcoCalc']]]
+  ['intoxication_5flevels_0',['intoxication_levels',['../class_alco_calc_1_1_alco_calc.html#a069cdc977e256c7377a6a9e9fdfc6d60',1,'AlcoCalc::AlcoCalc']]]
 ];
